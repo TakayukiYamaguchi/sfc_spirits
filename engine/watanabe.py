fe360668b0a126704bd94f68b70416f02695ea3c
TakayukiYamaguchi <taka.radical0921@icloud.com>
2020-06-20 19:15:52 +0900

    add:修正

--- engine/watanabe.py
+++ engine/watanabe.py
@@ -12,10 +12,10 @@ def dialogue(w):
         print('phase' + str(phase))
         word1 = x.load_w2v(w)
         sentence1 = x.make_sentence(word1)
-        print('student:' + sentence1)
+        print('watanabe:' + sentence1)
         word2 = y.tokenize(sentence1)
         sentence2 = y.make_sentence(word2)
-        print('watanabe' + sentence2)
+        print('sutudent' + sentence2)
         next_word_base = y.tokenize(sentence2)
         if not phase >= limit:
             dialogue(next_word_base)
